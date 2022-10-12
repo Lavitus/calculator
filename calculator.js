680b840baa004d62af4d830d3d66e2920fc3c0fb
LavituScuperino <samet71simsek@gmail.com>
2022-10-12 13:12:02 +0200

    full keyboard support & multiplying/dividing calculates first
    -display calculation input above result
    
    -backspace key/DEL button deletes input

--- calculator.js
+++ calculator.js
@@ -4,21 +4,55 @@ const calcNumberButtons = document.getElementsByClassName("calcNum");
 const calcEqualsButton = document.querySelector("#calcEquals")
 const calcOperatorButtons = document.querySelectorAll(".calcOperator")
 const calcClearButton = document.querySelector("#calcClear");
+const calcDelButton = document.getElementById("calcDel");
 
 let calcInputArray;
 let toggleOpVal = 1;
 let toggleEqualVal = 1;
+let displayType = "input";
+let operatorKeys = ["+","-","/","*"];
+
+currentCalcDisplay.addEventListener("keydown", 
+function(e) {
+        if (e.key <= 9) { 
+            numBtnClick(e.key)
+        } else if (operatorKeys.includes(e.key) === true ) {
+            operatorClick(e.key);
+        } else if (e.key === "Backspace") {
+            delInput();
+        }
+})
+
+currentCalcDisplay.addEventListener("click", () => {
+    currentCalcDisplay.focus()
+});
+
+calcDelButton.addEventListener("click", () => {
+    delInput();
+});
+// delete single number digits and operators
+function delInput() {
+    if (displayType === "input") {   
+        if ((currentCalcDisplay.textContent.slice(-2).replace(/ .$/, '') === "")) {
+            currentCalcDisplay.textContent = currentCalcDisplay.textContent.replace(/ .$/, '');
+            toggleOpBtn(1);
+        } else {
+            currentCalcDisplay.textContent = currentCalcDisplay.textContent.slice(0, -1);
+        }
+    }
+}
 
 // add number to input & toggle operators on if off
 for (let i = 0; i < calcNumberButtons.length; i++) {
     let btnNumValue = calcNumberButtons[i].textContent;
     calcNumberButtons[i].addEventListener("click", 
-    function () {   
+    function() {   
         numBtnClick(btnNumValue)
     })
 };
 
 function numBtnClick(btnNumValue) {
+    if (displayType === "input") {   
         calcInputArray = currentCalcDisplay.textContent.split(" ")
         if ((calcInputArray.length % 2) === 0) {
             currentCalcDisplay.textContent += ` ${btnNumValue}`;
@@ -27,6 +61,10 @@ function numBtnClick(btnNumValue) {
         }    
         toggleOpBtn(1);
         toggleEqualBtn(1);
+    } else {
+        clearInput();
+        numBtnClick(btnNumValue);
+    }
 }
 
 // add operator to input & toggle operators off
@@ -39,15 +77,17 @@ for (let i = 0; i < calcOperatorButtons.length; i++) {
 };
 
 function operatorClick(value) {
-    if (toggleOpVal === 1) {    
+    if (toggleOpVal === 1 && displayType === "input") {    
         calcInputArray = currentCalcDisplay.textContent.split(" ");
         if (calcInputArray.slice(-1)[0].length >= 1 && (calcInputArray.length % 2) === 1) {
+            if (value === "/") value = "÷";
+            else if (value === "*") value = "x";
             currentCalcDisplay.textContent += ` ${value}`;
-        } else if (((calcInputArray.length % 2) === 0) && value === "+" || value === "-") { 
+        } else if (((calcInputArray.length % 2) === 0) && value === "-") { 
             currentCalcDisplay.textContent += ` ${value}`;
             toggleOpBtn(0);
             toggleEqualBtn(0);
-        } else if (value === "+" || value === "-") {
+        } else if ((calcInputArray.length === 0) && value === "+" || value === "-") {
             currentCalcDisplay.textContent += `${value}`;
             toggleOpBtn(0);
             toggleEqualBtn(1);
@@ -63,14 +103,21 @@ function toggleOpBtn(a) {
         toggleOpVal = 0;
     }
 } 
-
+// listener for clear button, reset everything
 calcClearButton.addEventListener("click", 
 function () {
-    currentCalcDisplay.textContent = "";
+    clearInput();
     toggleOpBtn(1);
     toggleEqualBtn(1);
 });
 
+// clears input & sets display to number 
+function clearInput() {
+    currentCalcDisplay.textContent = "";
+    displayType = "input";
+}
+
+// toggles the function to get results, (toggleEqualVal 1 = enabled / 0 = disabled)
 function toggleEqualBtn(a) {
     if (a === 1 && toggleEqualVal === 0) {
         toggleEqualVal = 1;
@@ -85,35 +132,85 @@ let displayValue = 0;
 let a;
 let b;
 let op;
+let calcRuleArray = [];
+let calcRuleCond = ["÷","x"]
+
 // get and split input into array (pattern is: firstVal/sum, operator, secondVal) & calculate until end of array length
 calcEqualsButton.addEventListener("click", 
 function () {
-    if (toggleEqualVal === 1) {  
+    if (toggleEqualVal === 1 && displayType === "input") {  
+        currentCalcDisplay.textContent = parseInput(currentCalcDisplay.textContent)
         calcInputArray = currentCalcDisplay.textContent.split(" ");
-        
-        if ( calcInputArray.length >= 3) {
-            a = operate(calcInputArray[0], calcInputArray[1], calcInputArray[2])
-            
-            if ( calcInputArray.length >= 4)    
-                for (let i = 3; i < calcInputArray.length; i++) { 
-                    if ((i % 2) === 1) {
-                        op = calcInputArray[i];
-                    } else {
-                        b = calcInputArray[i];
-                        a = operate(a, op, b);
+        if ( calcInputArray.length >= 3) {    
+            if (calcRuleCond.some(i => calcInputArray.includes(i)) === true) {
+                calcInputArray.forEach((element, index) => {
+                    if(calcRuleCond.includes(element)){
+                        calcRuleArray.push(index);
                     }
-                } 
-                
-            if (calcInputArray.length >= 4 && (calcInputArray.length % 2) === 0) { 
-                currentCalcDisplay.textContent = `${a} ${op}`;
-            } else {
-                currentCalcDisplay.textContent = a;
+                })
+                calcRuleArray.forEach((calcIdx, index) => {
+                    op = calcInputArray[calcIdx]
+                    a = calcInputArray[calcIdx-1]
+                    b = calcInputArray[calcIdx+1]
+                    if (b == 0 && op === "÷") return error();
+                    else if (b === undefined) return calcRuleArray.pop();
+                    a = operate(a, op, b)
+                    calcInputArray.splice(calcIdx+1, 1, a)
+                })
+                calcRuleArray.reverse().forEach((calcIdx, index) => {
+                    calcInputArray.splice(calcIdx-1,2);
+                })
+                calcRuleArray = [];
             }
+        } else {
+            return;
         }
+
+        if ( calcInputArray.length >= 3 && displayType === "input") {
+            a = calcInputArray[0];
+            for (let i = 1; i < calcInputArray.length; i++) { 
+                if ((i % 2) === 1) {
+                        op = calcInputArray[i];
+                } else {
+                        b = calcInputArray[i];
+                        if (b == 0 && op === "÷") return error();
+                        a = operate(a, op, b);
+                }
+            } 
+        } else if (displayType === "errorText") {
+            return;
+        } else {
+            a = calcInputArray[0];
+            op = calcInputArray[1];
+        }
+        a = formatter.format(a);
+            
+        displayResults(calcInputArray) 
     }
 })
+// removes comma for calculations
+function parseInput(input) {
+    return input.replace(/,/g, '')
+  }
 
-// check operator and then calculate 
+// displays error text if calculation divides by 0
+function error() {
+    currentCalcDisplay.textContent = "You divided by 0! That's infinity!";
+    displayType = "errorText";
+}
+
+// displays result & get if operator at the end of the calculation
+function displayResults() {
+    if ((calcInputArray.length % 2) === 0) { 
+        lastResultDisplay.textContent = currentCalcDisplay.textContent.substring(0, currentCalcDisplay.textContent.length - 1) + " =";
+        currentCalcDisplay.textContent = `${a} ${op}`;
+    } else {
+        lastResultDisplay.textContent = currentCalcDisplay.textContent + " =";
+        currentCalcDisplay.textContent = a;
+    } 
+}
+
+// checks operator and then calculate 
 function operate (a, op, b) {
     if (op === "+") return add(+a,+b);
     else if (op === "-") return subtract(+a,+b);
@@ -137,3 +234,9 @@ function multiply (a,b) {
 function divide (a,b) {
     return a / b; 
 }
+
+// result max fraction digits 
+const formatter = new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: 0,      
+    maximumFractionDigits: 11,
+ });
